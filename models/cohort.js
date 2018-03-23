705434fbf3a562795a3253fd113ade3dbbd62fb2
Sebastian Hallum Clarke <s@zibity.com>
2018-03-23 11:16:24 -0400

    Starting building question builder

--- models/cohort.js
+++ models/cohort.js
@@ -1,5 +1,11 @@
 let mongoose = require('mongoose')
 
+const requiredTrimmedString = {
+  type: String,
+  required: true,
+  trim: true
+}
+
 let cohortSchema = mongoose.Schema({
   name: {
     type: String,
@@ -12,7 +18,18 @@ let cohortSchema = mongoose.Schema({
     trim: true
   },
   members: [String],
-  archived: Boolean
+  archived: Boolean,
+  demographicQuestions: [
+    {
+      title: requiredTrimmedString,
+      id: requiredTrimmedString,
+      kind: {
+        type: String,
+        enum: ['text', 'scale', 'choice']
+      },
+      options: [String]
+    }
+  ]
 })
 
 let Cohort = mongoose.model('Cohort', cohortSchema)
--- models/surveySet.js
+++ models/surveySet.js
@@ -1,28 +1,37 @@
 let mongoose = require('mongoose')
 let ObjectId = mongoose.Schema.Types.ObjectId
 
+const requiredTrimmedString = {
+  type: String,
+  required: true,
+  trim: true
+}
+
 let surveySetSchema = mongoose.Schema({
   cohort: {
     type: ObjectId,
     required: true,
     lowercase: true
   },
-  owner: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
+  owner: requiredTrimmedString,
+  name: requiredTrimmedString,
   surveyURL: {
     type: String,
     lowercase: true,
     trim: true
   },
-  sendDates: [Date]
+  sendDates: [Date],
+  surveyQuestions: [
+    {
+      title: requiredTrimmedString,
+      id: requiredTrimmedString,
+      kind: {
+        type: String,
+        enum: ['text', 'scale', 'choice']
+      },
+      options: [String]
+    }
+  ]
 })
 
 let SurveySet = mongoose.model('SurveySet', surveySetSchema)
