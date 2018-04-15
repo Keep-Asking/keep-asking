e4138ac0655fd4b0b550ea64635e9f358f9b15ce
Sebastian Hallum Clarke <s@zibity.com>
2018-04-14 20:23:33 -0400

    Tidying console logs

--- controllers/sendSurveyEmails.js
+++ controllers/sendSurveyEmails.js
@@ -2,6 +2,8 @@ const email = require('./email.js')
 
 const promises = []
 
+console.log('Running sendSurveyEmails script.')
+
 if (process.argv.length === 3 && process.argv[2] === 'remind') {
   promises.push(email.sendUnsentSurveyResponseReminderEmails())
 } else if (process.argv.length === 3 && process.argv[2] === 'request') {
@@ -12,7 +14,7 @@ if (process.argv.length === 3 && process.argv[2] === 'remind') {
 }
 
 Promise.all(promises).then(result => {
-  console.log(`Done (${result.length}). Quitting`)
+  console.log(`Done with sendSurveyEmails script. Quitting`)
   process.exit(0)
 }).catch(err => {
   console.error(err)
